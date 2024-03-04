94771a8ff2bc3123c8c22d61803ce7ab88671092
Lv Funing <153590199+Fnlyu@users.noreply.github.com>
2024-03-04 20:33:36 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=32454734&pid=679cbb0b11ad3bef6ce1eb542e4fd54a&tid=05b716d8e3d1a84cf404e1d61302e56b",
+    "https://www.maimemo.com/share/page?uid=34444135&pid=679cbb0b11ad3bef6ce1eb542e4fd54a&tid=fd793b310eab053b99e88379879ebf3c",
 
 
   ]
